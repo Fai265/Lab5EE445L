757e8ca71e2eed52bbf71d617acb4893673ad6a6
CooperCarnahan <coopcarn@gmail.com>
2017-10-09 00:45:34 -0500

    Moved PLL to the top of main

--- MAX5353TestMain.c
+++ MAX5353TestMain.c
@@ -102,11 +102,11 @@ void portF_Init(void){
 	GPIO_PORTF_AMSEL_R = 0;     // disable analog functionality on PF  
 }
 int main(void){
+	PLL_Init(Bus80Mhz);
   i=0;															 // This is the index of the song array
 	portF_Init();
   DAC_Init(0x1000);                  // initialize with command: Vout = Vref
   SysTick_Init();
-	PLL_Init(Bus80Mhz);
 	//EnableInterrupts();
 	play = 1;
 	note StarWarsNotes[100] = {	//Each measure on a different line
